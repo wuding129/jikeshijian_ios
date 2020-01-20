e0048c2e822133ac2cea77fba1cae7dee3db3d59
wuding <wuding@huishoubao.com.cn>
2020-01-20 12:39:06 +0800

    change navigationItem

--- SampleApp/SampleApp/ViewController.m
+++ SampleApp/SampleApp/ViewController.m
@@ -89,6 +89,8 @@
     viewController.view.backgroundColor = [UIColor whiteColor];
     viewController.navigationItem.title = @"内容";
     
+    viewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"右侧标题" style:UIBarButtonItemStylePlain target:self action:nil];
+    
     [self.navigationController pushViewController:viewController animated:YES];
 }
 
